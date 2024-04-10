f9c8ec12a8da962d5788bf2bf0ae40fd217e5ed4
iiTzAman <aman.giri2055@gmail.com>
2024-04-10 18:34:43 -0400

    Added final changes

--- BBQuotes/View/QuoteView.swift
+++ BBQuotes/View/QuoteView.swift
@@ -16,53 +16,52 @@ struct QuoteView: View {
     var body: some View {
         GeometryReader{ geo in
             ZStack{
-
+                
                 Image(show.lowercased().replacingOccurrences(of: " ", with: ""))
                     .resizable()
                     .frame(width: geo.size.width * 2.7, height: geo.size.height * 1.2)
                 VStack{
-                    Spacer(minLength: 100)
-                    
-                    switch viewModel.status{
-                    case .success(data: let data):
-                        Text("\"\(data.quote.quote)\"")
-                            .font(.title3)
-                            .minimumScaleFactor(0.5)
-                            .foregroundStyle(.white)
-                            .multilineTextAlignment(.center)
-                            .frame(width: geo.size.width / 1.4)
-                            .padding(20)
-                            .clipShape(.rect(cornerRadius: 20))
-                            .background(.black.opacity(0.7))
+                    VStack{
+                        Spacer(minLength: 100)
                         
-                        
-                        ZStack(alignment: .bottom){
-                            AsyncImage(url: data.character.images[0]) { image in
-                                image
-                                    .resizable()
-                                    .scaledToFill()
-                            } placeholder: {
-                                ProgressView()
+                        switch viewModel.status{
+                        case .success(data: let data):
+                            Text("\"\(data.quote.quote)\"")
+                                .font(.title3)
+                                .minimumScaleFactor(0.5)
+                                .foregroundStyle(.white)
+                                .multilineTextAlignment(.center)
+                                .frame(width: (geo.size.width / 1.4), height: 50)
+                                .padding(20)
+                                .clipShape(.rect(cornerRadius: 20))
+                                .background(.black.opacity(0.7))
+                            
+                            ZStack(alignment: .bottom){
+                                AsyncImage(url: data.character.images[0]) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFill()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                                
+                                Text(data.character.name)
+                                    .foregroundStyle(.white)
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                                    .background(.black.opacity(0.5))
                             }
                             .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
-
-                            Text(data.character.name)
-                                .foregroundStyle(.white)
-                                .frame(maxWidth: .infinity)
-                                .padding()
-                                .background(.black.opacity(0.5))
-                            
+                            .clipShape(.rect(cornerRadius: 20))
+                        case .fetching:
+                            ProgressView()
+                        default:
+                            EmptyView()
                         }
-                        .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
-                        .clipShape(.rect(cornerRadius: 20))
-                    case .fetching:
-                        ProgressView()
-                    default:
-                        EmptyView()
+                        
+                        Spacer()
                     }
-                    
-                    Spacer()
-                    
                     Button{
                         Task{
                             await viewModel.getData(for: show)
@@ -80,11 +79,16 @@ struct QuoteView: View {
                     Spacer(minLength:120)
                 }
                 .frame(width: geo.size.width, height: geo.size.height)
-
+                
             }
             .frame(width: geo.size.width, height: geo.size.height)
         }
         .ignoresSafeArea()
+        .onAppear(){
+            Task{
+                await viewModel.getData(for: show)
+            }
+        }
     }
 }
 
